Fix hidden units of relation network module

model.py
@@ -87,7 +87,7 @@ class RN(BasicModel):
         self.conv = ConvInputModel()
         
         ##(number of filters per object+coordinate of object)*2+question vector
-        self.g_fc1 = nn.Linear((24+2)*2+11, 256)
+        self.g_fc1 = nn.Linear(24 + 2 + 11, 256)
 
         self.g_fc2 = nn.Linear(256, 256)
         self.g_fc3 = nn.Linear(256, 256)
@@ -141,17 +141,19 @@ class RN(BasicModel):
         qst = torch.unsqueeze(qst, 2)
         
         # cast all pairs against each other
-        x_i = torch.unsqueeze(x_flat,1) # (64x1x25x26+11)
-        x_i = x_i.repeat(1,25,1,1) # (64x25x25x26+11)
+        # x_i = torch.unsqueeze(x_flat,1) # (64x1x25x26+11)
+        # x_i = x_i.repeat(1,25,1,1) # (64x25x25x26+11)
         x_j = torch.unsqueeze(x_flat,2) # (64x25x1x26+11)
         x_j = torch.cat([x_j,qst],3)
         x_j = x_j.repeat(1,1,25,1) # (64x25x25x26+11)
         
         # concatenate all together
-        x_full = torch.cat([x_i,x_j],3) # (64x25x25x2*26+11)
+        # x_full = torch.cat([x_i,x_j],3) # (64x25x25x2*26+11)
+        x_full = x_j
         
         # reshape for passing through network
-        x_ = x_full.view(mb*d*d*d*d,63)
+        # x_ = x_full.view(mb*d*d*d*d,63)
+        x_ = x_full.view(mb*d*d*d*d,37)
         x_ = self.g_fc1(x_)
         x_ = F.relu(x_)
         x_ = self.g_fc2(x_)